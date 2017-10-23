dcf08e2db3fd2fbcfb5a1bfac070c45a2e34c63f
AnnKrn <anakrn.carreon@gmail.com>
2017-10-23 15:15:59 -0500

    termina ejemplo arreglos

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var fila = [1,2,3,4];
 
-fila.push(5);
+fila.push(5); // agrega el elemento al final
+document.write(fila);
 
-fila.shift();
+fila.shift(); // quita el primer elemento del arreglo
+document.write(fila);
 
+fila.push('hola');
+document.write(fila);
+
+fila.unshift('perrito'); // agrega un elemento al principio del arreglo
 document.write(fila);
